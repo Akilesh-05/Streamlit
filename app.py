445efed8aff310696a3ad79d2e82bb464603d4f1
rajesh-9791 <akileshnagarajan456@gmail.com>
2025-03-03 10:57:54 +0530

    commited

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+import streamlit as st
+
+st.header("My Dashboard")
+st.header("Data Visualization", anchor="viz")
+
+st.markdown("# heading")
+st.markdown("## subheading")
+st.markdown("[Google](https://www.google.com) for web search.")
+
+st.header("Streamlit")
+st.subheader("Streamlit subheading")
+
+st.write("Welcome!")
+
+if st.button("Click me!"):
+    st.write("Hello Man!!")
+else:
+    st.write("Noo...")
